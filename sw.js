eba0addabf730a9fdb34ec3268e9e8b176fcbf17
Nestor-92 <nesart50@gmail.com>
2026-09-24 20:00:13 +0200

    Actualise le cache PWA pour la v10 OCR

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="omr-eps-v9";
-const LOCAL=["./","./index.html","./manifest.webmanifest","./v8.js?v=9"];
+const CACHE="omr-eps-v10";
+const LOCAL=["./","./index.html","./manifest.webmanifest","./v8.js?v=10"];
 self.addEventListener("install",event=>{
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL).catch(()=>{})));

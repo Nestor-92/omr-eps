5371194c08f95cfb4d3386477160a2bfd3c64f8a
Nestor-92 <nesart50@gmail.com>
2026-09-24 19:11:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE="omr-eps-v7";
+const LOCAL=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL).catch(()=>{})));
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener("fetch",event=>{
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(
+      fetch(event.request)
+        .then(resp=>{
+          const copy=resp.clone();
+          caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+          return resp;
+        })
+        .catch(()=>caches.match(event.request).then(r=>r||caches.match("./index.html")))
+    );
+  }
+});
